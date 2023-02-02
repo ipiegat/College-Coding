14b8f1ac0d5bb1cb0c7e4d11f187c4fb79700b34
Isaac Piegat <ipiegat@icloud.com>
2023-02-02 13:51:21 -0500

    edit

--- CSE2050 Lab.py/Lab2.py
+++ CSE2050 Lab.py/Lab2.py
@@ -3,44 +3,46 @@ class Polygon:
         self.nbr_sides = nbr_sides
 
     def whoamI(self):
-        pass
+        if self.nbr_sides == 3:
+            return "Triangle"
+        if self.nbr_sides == 4:
+            return "Rectangle"
 
     def howmanysides(self):
-        pass
+        return self.nbr_sides
+
+    def area(self):
+        return "No area"
+
+    def perimeter(self):
+        return "No perimeter"
+
 
 class Rectangle(Polygon):
     def __init__(self, breadth, length):
         self.breadth = breadth
         self.length = length
-    
-    def area(self, breadth, length):
-        pass
-        area = breadth * length
-        #return area
-    
-    def perimeter(self, breadth, length):
-        pass
-        #perimeter = (breadth*2)+(length*2)
-        #return perimeter
+
+    def area(self):
+        area = self.breadth * self.length
+        return area
+
+    def perimeter(self):
+        perimeter = 2 * (self.length + self.breadth)
+        return perimeter
+
 
 class Triangle(Polygon):
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
-    
-    def area(self, a, b):
-        pass
-        #area = 1/2(a*b)
-        #return area
-    
-    def perimeter(self, a, b, c):
-        pass
-        #perimeter = a + b + c
-        #return perimeter
-    
 
+    def area(self):
+        s = (self.a + self.b + self.c) / 2
+        area = (s * (s - self.a) * (s - self.b) * (s - self.c)) ** (1 / 2)
+        return area
 
-
-
-
+    def perimeter(self):
+        perimeter = self.a + self.b + self.c
+        return perimeter
---
+++ CSE2050 Lab.py/TestLab2.py
@@ -0,0 +1,31 @@
+import unittest
+from Lab2 import Polygon, Triangle, Rectangle
+
+
+class TestLab2(unittest.TestCase):
+    def test_init(self):
+
+        a1 = Polygon(3)
+        self.assertEqual(a1.whoamI(), "Triangle")
+        self.assertEqual(a1.howmanysides(), 3)
+        a2 = Polygon(4)
+        self.assertEqual(a2.whoamI(), "Rectangle")
+        self.assertEqual(a2.howmanysides(), 4)
+
+    def test_area(self):
+
+        t1 = Triangle(3, 4, 5)
+        self.assertEqual(t1.area(), 6)
+        r1 = Rectangle(2, 4)
+        self.assertEqual(r1.area(), 8)
+
+    def test_perimeter(self):
+
+        t1 = Triangle(3, 4, 5)
+        self.assertEqual(t1.perimeter(), 12)
+        r1 = Rectangle(2, 4)
+        self.assertEqual(r1.perimeter(), 12)
+
+
+if __name__ == "__main__":
+    unittest.main()
