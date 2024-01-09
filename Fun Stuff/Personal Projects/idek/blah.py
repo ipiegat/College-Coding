edcbf5ada99d34cae32be156df1618dcb35ea0f5
ipiegat <ipiegat@icloud.com>
2024-01-09 13:19:46 -0500

    2024 update 2

--- Fun Stuff/Personal Projects/idek/blah.py
+++
@@ -1,9 +0,0 @@
-def file_writer(file, min, max):
-
-    for num in range(min, max):
-        with open(file, 'w') as file:
-            file.write(str(num))
-
-if __name__ == "__main__":
-    test = file_writer(r"C:\Users\ipieg\OneDrive\College-Coding\Fun Stuff\Personal Projects\idek\some.txt", 52, 83)    
-    
