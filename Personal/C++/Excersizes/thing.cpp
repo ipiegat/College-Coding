d92ab6d7680247852e553201cda84d45813bff99
Isaac Piegat <ipiegat@icloud.com>
2026-01-10 12:01:29 -0500

    singleton

--- Personal/C++/Excersizes/thing.cpp
+++ Personal/C++/Excersizes/thing.cpp
@@ -14,7 +14,31 @@ class RAII {
 
 };
 
+class Singleton {
+    public:
+        // delete copy constructor
+        Singleton(const Singleton&) = delete;
+        // delete move constructor
+        Singleton(Singleton&&) = delete;
+        // delete copy assignment operator
+        Singleton& operator = (const Singleton&) = delete;
+        // delete move assignment operator
+        Singleton& operator = (Singleton&&) = delete;
+
+
+        static Singleton& GetInstance() {
+            static Singleton instance;
+            return instance;
+        }
+
+    private:
+        // default constructor
+        // make private such that only one instance can be created at a time
+        Singleton() = default;
+
+};
+
 int main() {
-    RAII a;
-    cout << "3" << endl;
+    Singleton& a = Singleton::GetInstance();
 }
+
